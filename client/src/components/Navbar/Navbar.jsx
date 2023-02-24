062f577ff61667ec46e7eb3d6d29df1c91d861c1
planetoida90210 <galeriakoloru90210@gmail.com>
2023-02-24 15:50:58 +0100

    navbar ui

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import "./Navbar.scss";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -21,17 +22,57 @@ const Navbar = () => {
             <KeyboardArrowDownIcon />
           </div>
           <div className="item">
-            <Link to="/products/1">Women</Link>
+            <Link className="link" to="/products/1">
+              Women
+            </Link>
           </div>
           <div className="item">
-            <Link to="/products/1">Man</Link>
+            <Link className="link" to="/products/2">
+              Man
+            </Link>
           </div>
           <div className="item">
-            <Link to="/products/1">Children</Link>
+            <Link className="link" to="/products/3">
+              Children
+            </Link>
           </div>
         </div>
-        <div className="center"></div>
-        <div className="right"></div>
+        <div className="center">
+          <Link className="link" to="/">
+            GK SKLEP
+          </Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link className="link" to="/">
+              Homepage
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              About
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Contact
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Stores
+            </Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
